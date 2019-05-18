8715d574cc0ce8d87cfe482384caeba9296d6a9b
veaba <908662421@qq.com>
2019-05-18 18:37:37 +0800

    update 2019年5月18日18:37:35

---
+++ casting.go
@@ -0,0 +1,24 @@
+/**
+@desc 类型转换
+@语法 type_name(表达式)
+*/
+package main
+
+import "fmt"
+
+func main()  {
+	//int-float
+	intToFloat()
+}
+
+func intToFloat() {
+	var sum =999
+	var mean float32
+	mean = float32(sum)
+
+	mean = float32(sum)/float32(7)
+	fmt.Println(mean) //142.71428
+
+	var i =  float32(9)/7
+	fmt.Println(i) //1.2857143
+}
--- range.go
+++ range.go
@@ -9,12 +9,20 @@ func main()  {
 	nums := []int {998,65,6856,6}//定义一个切片
 	sum:=0
 
-	// 这个 "_"干嘛的？不需要索引，所以使用空白符 “_”省略
-	for index,num:=range nums {
-		fmt.Println("num:",num)
-		fmt.Println("index:",index)
+	// 这个 "_"干嘛的？不需要索引，所以使用空白符 “_”省略 迭代
+	for _,num:=range nums {
+		//fmt.Println("num:",num)
+		//fmt.Println("index:",index)
 		sum+=num
 	}
 
 	fmt.Println("sum:",sum)
+
+	//range 可以在map的键值对上。
+	kvs:=map[string]string{"who":"IG输了","MSI2019":"舒服了"}
+
+	fmt.Println(kvs)//map[MSI2019:舒服了 who:IG输了]
+	for k,v:=range kvs {
+		fmt.Printf("%s -> %s \n",k,v)
+	}
 }
